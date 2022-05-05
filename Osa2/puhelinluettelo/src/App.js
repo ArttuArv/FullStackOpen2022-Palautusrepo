0120cfc5f976d9c0d1aafecb6e65532a10dff440
Arttu Arvelin <t9arar00@students.oamk.fi>
2022-05-05 10:52:10 +0300

    Puhelinluetteloon tehty db.json-tietokanta ja tiedonhaku axiosilla

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({nameFilter, handleNameFilterChange}) => {
   return (
@@ -48,18 +49,24 @@ const Persons = ({personsToShow}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const[filteredPersons, setFilteredPersons] = useState([]);
 
   // Lomakkeen kenttien kontrollointiin
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [nameFilter, setNameFilter] = useState('');
+
+  // Haetaan palvelimelta tiedot ja asetetaan ne persons-taulukkoon
+  useEffect(() => {
+    console.log("Effect");
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("promise fulfilled");
+        setPersons(response.data);
+      })
+  }, []);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();    
