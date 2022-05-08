80a49fc619625f916dcb47900cd673821db14bd6
Arttu Arvelin <t9arar00@students.oamk.fi>
2022-05-08 13:36:35 +0300

    Maiden tiedot etenee

--- Osa2/maiden-tiedot/src/App.js
+++ Osa2/maiden-tiedot/src/App.js
@@ -1,47 +1,58 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+const dotenv = require('./process.env')
 
-const CountryInfo = ({ countries }) => {
-  console.log("CountryInfo-Komponentti");
-  if (countries.length === 1) {
-    console.log("CountryInfo: ", countries[0].name);
+
+const CountryInfo = ({ countries, buttonClick }) => {
+  //console.log("CountryInfo-Komponentti", countries);
+
+  if (countries.length === 1 && countries[0].name !== "Too many matches, specify another filter" ) {    
+
     return (
-      <div>
+      <>
         <h2>{countries[0].name}</h2>
-        <p><b>Capital:</b> {countries[0].capital}</p>
-        <p><b>Area:</b>{countries[0].area}</p>
+        <p><b>Capital: </b>{countries[0].capital}</p>
+        <p><b>Area: </b>{countries[0].area}</p>
+        <h3>Languages</h3>
+        <ul>
+          {countries[0].languages.map(({name}) =>
+            <li key = {name}>
+              {name} 
+            </li>
+          )}
+        </ul>
         <img src = {countries[0].flags.png} alt = "Lippu" />
-
-      </div>
+      </>
     );
   } else {      
     return (
       <>
-        <ul>
-            {countries.map(country => 
-            <li key={country.name}>
-              {country.name}
-            </li>
-            )}
-          </ul>
+        <dl>
+          {countries.map(country => 
+          <dt key={country.name}>
+            {country.name} 
+            <button onClick = {buttonClick} >
+              Show
+            </button>
+          </dt>
+          )}
+        </dl>
       </>
     );
   }
 }
 
-
 const App = () => {
   const [allCountries, setAllCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
+  const [weather, setWeather] = useState([]);
 
   const [newCountry, setNewCountry] = useState('');
 
   useEffect(() => {
-    //console.log("effect");
     axios
       .get('https://restcountries.com/v2/all')
       .then(response => {
-      //  console.log("promise fulfilled");
         setAllCountries(response.data);
       })
       .catch(error => {
@@ -53,7 +64,7 @@ const App = () => {
     setNewCountry(event.target.value);
 
     let countryToUpperCase = event.target.value.toUpperCase();
-    let filteredCountries = allCountries.filter(({name}) => name.toUpperCase().includes(countryToUpperCase));
+    let filteredCountries = allCountries.filter(({ name }) => name.toUpperCase().includes(countryToUpperCase));
 
     // Tarkatetaan onko suodatetussa taulukossa pituutta enemmän kuin 10 ja onko syöttökentässä kirjaimia.
     // Asetetaan filteredCountries taulukkoon yksi nimikenttä ja arvoksi vihjeen antava viesti.
@@ -63,27 +74,43 @@ const App = () => {
 
     setFilteredCountries(filteredCountries);
   }
+
+  const handleButtonCLick = (event) => {
+    const country = allCountries.find(({name}) => name === event.target.parentNode.firstChild.nodeValue);
+    setFilteredCountries([country]);
+    setNewCountry('');
+  }  
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${"Helsinki"}&appid=${dotenv.WEATHER_APIKEY}`)
+      .then(response => {
+        console.log("response.data", response.data);
+        setWeather(response.data);
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }, []);
   
   const countriesToShow = filteredCountries.length > 0
     ? filteredCountries
     : allCountries;
 
-  console.log("FilteredCountries Length: ", filteredCountries.length);
-  console.log("AllCountries Length: ", allCountries.length);
+  //console.log("FilteredCountries Length: ", filteredCountries.length);
+  //console.log("AllCountries Length: ", allCountries.length);
+  console.log("RENDERING...");
+  console.log("CountriesToShow: ", countriesToShow);
+  console.log("FilteredCountries: ", filteredCountries);
+  console.log("Weather: ", weather);
+  console.log(dotenv.WEATHER_APIKEY);
 
   return (
     <div>
       <h2>Countries</h2>
       Find countries
       <input onChange = {handleInputChange} value = {newCountry} />
-      <ul>
-        {countriesToShow.map(country => 
-        <li key={country.name}>
-          {country.name}
-        </li>
-        )}
-      </ul>
-      <CountryInfo countries={countriesToShow} />
+      <CountryInfo countries = {countriesToShow} buttonClick = {handleButtonCLick} />
     </div>
   );
 }
